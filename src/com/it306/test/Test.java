33619620545d0f989aaf06f31eb6d9d6713e46ab
Amith Kini <kini.amit.22@gmail.com>
2017-03-11 15:18:33 +0530

    Initial commit

---
+++ src/com/it306/test/Test.java
@@ -0,0 +1,12 @@
+package com.it306.test;
+
+/**
+ * This is a test class file created at the start of the project.
+ * This can be deleted without any dependency issues.
+ * Created by Amith Kini on 11-03-2017.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
