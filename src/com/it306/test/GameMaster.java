e0ae9bf4919bf0a7520602bf370d7110bbe9d6b9
Amith Kini <kini.amit.22@gmail.com>
2017-03-22 22:54:55 +0530

    Update Start GUI to add 2 to 4 players.

--- src/com/it306/test/GameMaster.java
+++ src/com/it306/test/GameMaster.java
@@ -6,11 +6,23 @@ import java.util.ArrayList;
 public class GameMaster {
 	private static GameMaster gameMaster;
 	private final int MAX_PLAYERS = 8;
-//	private Die[] die;
+	private Dice[] dice;
 	private Board gameBoard;
 	private int initMoney;
 	private ArrayList<Player> players = new ArrayList<Player>();
 	private int turn = 0;
+	
+	public static GameMaster instance() {
+		if (gameMaster == null) {
+			gameMaster = new GameMaster();
+		}
+		return gameMaster;
+	}
+	
+	public GameMaster() {
+		initMoney = 1500;
+		dice = new Dice[]{new Dice(), new Dice()};
+	}
 	
 	
 	
--- src/com/it306/test/UI/Start.java
+++ src/com/it306/test/UI/Start.java
@@ -10,6 +10,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
 import javax.swing.JTextField;
 //import javax.swing.LookAndFeel;
 //import javax.swing.SwingUtilities;
@@ -17,12 +20,18 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.JButton;
 import java.awt.Color;
+import javax.swing.JComboBox;
 
 public class Start {
 
 	private JFrame frame;
 	private JTextField textField;
 	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JLabel lblPlayer_2;
+	private JLabel lblPlayer_3;
+	private static Integer[] plrs = {2,3,4};
 
 	/**
 	 * Launch the application.
@@ -33,6 +42,38 @@ public class Start {
 				try {
 					Start window = new Start();
 					window.frame.setVisible(true);
+					//The main code goes here
+					JComboBox<Integer> comboBox = new JComboBox<Integer>(plrs);
+					comboBox.setBounds(226, 145, 65, 22);
+					window.frame.getContentPane().add(comboBox);
+					
+					comboBox.addItemListener(new ItemListener(){
+
+						@Override
+						public void itemStateChanged(ItemEvent arg0) {
+							if ((int)arg0.getItem() == 2) {
+								window.lblPlayer_2.setVisible(false);
+								window.textField_2.setVisible(false);
+								window.lblPlayer_3.setVisible(false);
+								window.textField_3.setVisible(false);
+							}
+							else if ((int)arg0.getItem() == 3) {
+								window.lblPlayer_2.setVisible(true);
+								window.textField_2.setVisible(true);
+								window.lblPlayer_3.setVisible(false);
+								window.textField_3.setVisible(false);
+							}
+							else if ((int)arg0.getItem() > 3) {
+								window.lblPlayer_2.setVisible(true);
+								window.textField_2.setVisible(true);
+								window.lblPlayer_3.setVisible(true);
+								window.textField_3.setVisible(true);
+							}
+						}
+						
+					});
+					
+					//Ends here
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -63,7 +104,7 @@ public class Start {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 715, 476);
+		frame.setBounds(100, 100, 715, 585);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -75,43 +116,64 @@ public class Start {
 		frame.getContentPane().add(lblBusiness);
 		
 		JLabel lblPlayer = new JLabel("Player 1");
-		lblPlayer.setBounds(370, 181, 56, 16);
+		lblPlayer.setBounds(370, 148, 56, 16);
 		frame.getContentPane().add(lblPlayer);
 		
 		textField = new JTextField();
-		textField.setBounds(370, 211, 130, 22);
+		textField.setBounds(370, 181, 130, 22);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
-		JLabel lblPlr1 = new JLabel("");
-		lblPlr1.setLabelFor(textField);
-		lblPlr1.setBounds(519, 217, 56, 16);
-		frame.getContentPane().add(lblPlr1);
-		
 		JLabel lblPlayer_1 = new JLabel("Player 2");
-		lblPlayer_1.setBounds(370, 246, 56, 16);
+		lblPlayer_1.setBounds(370, 216, 56, 16);
 		frame.getContentPane().add(lblPlayer_1);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(370, 275, 130, 22);
+		textField_1.setBounds(370, 245, 130, 22);
 		frame.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 		
-		JLabel lblPlr2 = new JLabel("");
-		lblPlr2.setLabelFor(textField_1);
-		lblPlr2.setBounds(519, 278, 56, 16);
-		frame.getContentPane().add(lblPlr2);
-		
 		JButton btnStart = new JButton("Start!");
-		btnStart.setBounds(370, 333, 97, 25);
+		btnStart.setBounds(370, 434, 108, 73);
 		frame.getContentPane().add(btnStart);
 		
 		JButton btnRules = new JButton("Rules");
-		btnRules.setBounds(40, 333, 97, 25);
+		btnRules.setBounds(42, 458, 97, 25);
 		frame.getContentPane().add(btnRules);
 		
 		JButton btnAbout = new JButton("About");
-		btnAbout.setBounds(171, 333, 97, 25);
+		btnAbout.setBounds(174, 458, 97, 25);
 		frame.getContentPane().add(btnAbout);
+		
+		JLabel lblSelectTheNo = new JLabel("Select the no. of players");
+		lblSelectTheNo.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblSelectTheNo.setBounds(40, 141, 183, 29);
+		frame.getContentPane().add(lblSelectTheNo);
+		
+		lblPlayer_2 = new JLabel("Player 3");
+		lblPlayer_2.setVisible(false);
+		lblPlayer_2.setBounds(370, 283, 56, 16);
+		frame.getContentPane().add(lblPlayer_2);
+		
+		textField_2 = new JTextField();
+		textField_2.setVisible(false);
+		textField_2.setColumns(10);
+		textField_2.setBounds(370, 313, 130, 22);
+		frame.getContentPane().add(textField_2);
+		
+		lblPlayer_3 = new JLabel("Player 4");
+		lblPlayer_3.setVisible(false);
+		lblPlayer_3.setBounds(370, 348, 56, 16);
+		frame.getContentPane().add(lblPlayer_3);
+		
+		textField_3 = new JTextField();
+		textField_3.setVisible(false);
+		textField_3.setColumns(10);
+		textField_3.setBounds(370, 377, 130, 22);
+		frame.getContentPane().add(textField_3);
+		
+		JLabel label = new JLabel("");
+		label.setBounds(54, 230, 237, 137);
+		frame.getContentPane().add(label);
 	}
 }
