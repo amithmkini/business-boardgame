31e7e13f5355a9068d7b792902768651e29c0372
Amith Kini <kini.amit.22@gmail.com>
2017-03-31 14:44:04 +0530

    Change the type of argument for Property info GUI.

--- src/com/it306/test/UI/PropertyInfo.java
+++ src/com/it306/test/UI/PropertyInfo.java
@@ -1,5 +1,12 @@
 package com.it306.test.UI;
 
+/**
+ * This class shows the property information when the user clicks one of the
+ * labels on the board. It should show properties of all the cells.
+ * 
+ * @author Amith Kini
+ */
+
 import com.it306.test.*;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -17,15 +24,15 @@ public class PropertyInfo {
 	 * @param Property class 
 	 * Name, Value, Owner, Colour, Rent
 	 */
-	public PropertyInfo(Property property) {
-		initialize(property);
+	public PropertyInfo(Cell cell) {
+		initialize(cell);
 		frame.setVisible(true);
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize(Property property) {
+	private void initialize(Cell cell) {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 469, 419);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -41,7 +48,7 @@ public class PropertyInfo {
 		lblName.setBounds(34, 116, 56, 20);
 		frame.getContentPane().add(lblName);
 		
-		JLabel label = new JLabel(property.getName());
+		JLabel label = new JLabel(cell.getName());
 		label.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		label.setLabelFor(lblName);
 		label.setBounds(110, 115, 228, 20);
@@ -52,7 +59,7 @@ public class PropertyInfo {
 		lblValue.setBounds(34, 159, 56, 20);
 		frame.getContentPane().add(lblValue);
 		
-		JLabel label_1 = new JLabel(String.valueOf((property.getValue())));
+		JLabel label_1 = new JLabel(String.valueOf((cell.getValue())));
 		label_1.setLabelFor(lblValue);
 		label_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		label_1.setBounds(110, 159, 228, 20);
@@ -63,7 +70,7 @@ public class PropertyInfo {
 		lblOwner.setBounds(34, 199, 56, 20);
 		frame.getContentPane().add(lblOwner);
 		
-		JLabel label_2 = new JLabel(property.getOwner());
+		JLabel label_2 = new JLabel(cell.getOwner());
 		label_2.setLabelFor(lblOwner);
 		label_2.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		label_2.setBounds(110, 199, 228, 20);
@@ -74,7 +81,7 @@ public class PropertyInfo {
 		lblColour.setBounds(34, 240, 56, 20);
 		frame.getContentPane().add(lblColour);
 		
-		JLabel label_3 = new JLabel(property.getColourGroup());
+		JLabel label_3 = new JLabel(cell.getColourGroup());
 		label_3.setLabelFor(lblColour);
 		label_3.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		label_3.setBounds(110, 243, 228, 20);
@@ -85,7 +92,7 @@ public class PropertyInfo {
 		lblRent.setBounds(34, 276, 56, 20);
 		frame.getContentPane().add(lblRent);
 		
-		JLabel label_4 = new JLabel(String.valueOf(property.getRent()));
+		JLabel label_4 = new JLabel(String.valueOf(cell.getRent()));
 		label_4.setLabelFor(lblRent);
 		label_4.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		label_4.setBounds(110, 276, 228, 20);
