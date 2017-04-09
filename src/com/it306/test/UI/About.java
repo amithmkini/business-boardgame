88adccb53028f87d7b3f32e1a56fafa7f007fdb5
Amith Kini <kini.amit.22@gmail.com>
2017-04-10 01:18:35 +0530

    Bug fixes.

--- src/com/it306/test/UI/About.java
+++ src/com/it306/test/UI/About.java
@@ -25,7 +25,7 @@ public class About {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 542, 406);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblAbout = new JLabel("About");
